a0c5cea430677b957d03d041ea52b13074063039
syayid <syayidsyayid262@gmail.com>
2026-03-31 10:19:18 +0700

    Create usercontroller

---
+++ app/Http/Controllers/Api/User/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\StoreUserRequest;
+use App\Http\Requests\User\UpdateUserRequest;
+use App\Models\User;
+use App\Services\UserService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UserController extends Controller
+{
+    protected UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'status' => true,
+            'message' => 'Semua data user berhasil diambil',
+            'data' => $this->userService->listUsers()
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request): JsonResponse
+    {
+        $user = $this->userService->createUser($request->validated());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User berhasil dibuat',
+            'data' => $user
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user): JsonResponse
+    {
+        $user->load('role');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data user berhasil diambil',
+            'data' => $user
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user): JsonResponse
+    {
+        $user = $this->userService->updateUser($user, $request->validated());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User berhasil diperbarui',
+            'data' => $user
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        $this->userService->deleteUser($user);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User berhasil dihapus',
+        ], 200);
+    }
+}
