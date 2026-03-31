730276cd2246b0d3ba1d2813def5252c32d179d2
syayid <syayidsyayid262@gmail.com>
2026-03-31 19:38:44 +0700

    menambahkan api users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\Role\RoleController;
+use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('roles', RoleController::class);
+
+Route::apiResource('users', UserController::class);
