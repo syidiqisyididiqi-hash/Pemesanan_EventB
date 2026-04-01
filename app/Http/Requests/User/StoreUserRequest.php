64ed063e478c6b4f48120cb1f888665a8a0eae6e
syayid <syayidsyayid262@gmail.com>
2026-04-01 23:10:11 +0700

    Update request user

--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -20,7 +20,7 @@ class StoreUserRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-    //  * @return array<string, ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
