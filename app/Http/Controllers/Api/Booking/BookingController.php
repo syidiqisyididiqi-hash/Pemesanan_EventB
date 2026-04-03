219ec8887a34ce081752a1580e384932f3b13503
syayid <syayidsyayid262@gmail.com>
2026-04-03 10:16:41 +0700

    Create booking controller

---
+++ app/Http/Controllers/Api/Booking/BookingController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api\Booking;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Booking\StoreBookingRequest;
+use App\Http\Requests\Booking\UpdateBookingRequest;
+use App\Models\Booking;
+use App\Services\BookingService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Nette\Utils\Json;
+
+class BookingController extends Controller
+{
+    protected BookingService $bookingService;
+
+    public function __construct(BookingService $bookingService)
+    {
+        $this->bookingService = $bookingService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json(
+            $this->bookingService->listBookings()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookingRequest $request): JsonResponse
+    {
+        $booking = $this->bookingService->createBooking($request->validated());
+
+        return response()->json([
+            'message' => 'Booking berhasil dibuat',
+            'data' => $booking
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Booking $booking): JsonResponse
+    {
+        return response()->json($booking);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookingRequest $request, Booking $booking): JsonResponse
+    {
+        $booking = $this->bookingService->updateBooking($booking, $request->validated());
+
+        return response()->json([
+            'message' => 'Booking berhasil diperbarui',
+            'data' => $booking
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Booking $booking): JsonResponse
+    {
+        $this->bookingService->deleteBooking($booking);
+
+        return response()->json(null, 204);
+    }
+}
