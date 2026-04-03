018bcabf33235e2f82b7a2bec42a799364332467
syayid <syayidsyayid262@gmail.com>
2026-04-03 16:45:46 +0700

    Cretae ticket controller

---
+++ app/Http/Controllers/Api/Ticket/TicketController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api\Ticket;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Ticket\StoreTicketRequest;
+use App\Http\Requests\Ticket\UpdateTicketRequest;
+use App\Models\Ticket;
+use App\Services\TicketService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    protected TicketService $ticketService;
+
+    public function __construct(TicketService $ticketService)
+    {
+        $this->ticketService = $ticketService;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(
+            $this->ticketService->listTickets()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTicketRequest $request): JsonResponse
+    {
+        $ticket = $this->ticketService->createTicket($request->validated());
+
+        return response()->json([
+            'message' => 'Tiket berhasil dibuat',
+            'data' => $ticket
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ticket $ticket): JsonResponse
+    {
+        $ticket->load(['booking', 'booking.user', 'booking.event']);
+
+        return response()->json([
+            'message' => 'Tiket ditemukan',
+            'data' => $ticket
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTicketRequest $request, Ticket $ticket): JsonResponse
+    {
+        $ticket = $this->ticketService->updateTicket($ticket, $request->validated());
+
+        return response()->json([
+            'message' => 'Tiket berhasil diperbarui',
+            'data' => $ticket
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ticket $ticket): JsonResponse
+    {
+        $this->ticketService->deleteTicket($ticket);
+
+        return response()->json(null, 204);
+    }
+}
