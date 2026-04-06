9f72c247e75bab7dd58cce89e125e6abfe577607
syayid <syayidsyayid262@gmail.com>
2026-04-06 22:30:32 +0700

    Create notification request

---
+++ app/Http/Requests/Notification/StoreNotificationRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Notification;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreNotificationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => ['required', 'exists:users,id'],
+            'title' => ['required', 'string', 'max:100'],
+            'message' => ['required', 'string'],
+            'is_read' => ['sometimes', 'boolean'],
+        ];
+    }
+}
