f6a1108218d6904b19193ef23362fc020ce122f4
syayid <syayidsyayid262@gmail.com>
2026-04-06 22:40:58 +0700

    Create api notification

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\Booking\BookingController;
 use App\Http\Controllers\Api\Category\CategoryController;
 use App\Http\Controllers\Api\Event\EventController;
 use App\Http\Controllers\Api\Image\ImageController;
+use App\Http\Controllers\Api\Notification\NotificationController;
 use App\Http\Controllers\Api\Payment\PaymentController;
 use App\Http\Controllers\Api\Review\ReviewController;
 use App\Http\Controllers\Api\Role\RoleController;
@@ -25,3 +26,5 @@ Route::apiResource('payments', PaymentController::class);
 Route::apiResource('images', ImageController::class);
 
 Route::apiResource('reviews', ReviewController::class);
+
+Route::apiResource('notifications', NotificationController::class);
